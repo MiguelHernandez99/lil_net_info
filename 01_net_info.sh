b0d1e3ab48d8dc5f49205096b75671f26e4af356
user90 <loco056.eh@gmail.com>
2026-09-11 09:38:19 -0600

    programa para info de la RED

--- 01_net_info.sh
+++ 01_net_info.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+#limpiar pantalla
+clear_screen() {
+	clear
+	echo "NET INFO"
+	echo "======================================="
+}
+
+#1. Checar conexion a internet
+#2. Mostar IPs
+#3.Escanear puertos locales abiertos
+
+#Bucle para menu
