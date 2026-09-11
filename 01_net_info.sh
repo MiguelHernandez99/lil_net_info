35386599c896b2e2c22726451a77f7a48ac474ab
user90 <loco056.eh@gmail.com>
2026-09-11 12:28:05 -0600

    pequeño menu, funcionamiento basico

--- 01_net_info.sh
+++ 01_net_info.sh
@@ -7,7 +7,41 @@ clear_screen() {
 }
 
 #1. Checar conexion a internet
+check_internet() {
+	clear_screen
+	echo "Verificando conexion a internet"
+	read -p "Presiona Enter para continuar"
+
+}
 #2. Mostar IPs
+show_ips() {
+	clear_screen
+	echo "Obeteniendo IPs"	
+	read -p "Presiona Enter para continuar"
+
+}
 #3.Escanear puertos locales abiertos
+check_ports() {
+	clear_screen
+	echo "Escaneando puertos locales activos"
+	read -p "Presiona Enter para continuar"
+}
 
 #Bucle para menu
+while true; do
+	clear_screen
+	echo "1. Conexion a internet"
+	echo "2. Interfaces e IPs"
+	echo "3, Puertos locales"
+	echo "4. Salir"
+	read -p "Selecciona una opcion 1-4: " opcion
+
+	case $opcion in
+		1) check_internet ;;
+		2) show_ips ;;
+		3) check_ports ;;
+		4) clear; echo "Saliendo..."; exit 0 ;;
+		*) echo "Opcion no valida" ;;
+	esac
+done
+
